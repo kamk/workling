63d498ef72c05b392e50aa94edff7c6f8e389606
Frederik Fix <ich@derfred.com>
2011-11-07 13:52:37 +0100

    this is needed for ruby 1.9

--- lib/workling/base.rb
+++ lib/workling/base.rb
@@ -38,7 +38,7 @@ module Workling
 
     # identify the queue for a given method
     def self.queue_for(method)
-      if self.exposed_methods.has_key?(method)
+      if self.exposed_methods.has_key?(method.to_s)
         self.exposed_methods[method]
       else
         "#{ self.to_s.tableize }/#{ method }".split("/").join("__") # Don't split with : because it messes up memcache stats
